baf845d838717510dc6f650d3059a0d88f56bb73
ShaswataG <gshaswata1@gmail.com>
2025-03-22 01:18:19 +0530

    refactor: change indentation

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -76,34 +76,34 @@ const QuestionCard = () => {
           className="max-w-[20rem] py-6"
           placeholder="Type your answer here..."
         />
-          <Button
-              onClick={handleSubmit}
-              disabled={isSubmitting}
+        <Button
+            onClick={handleSubmit}
+            disabled={isSubmitting}
             className="group relative inline-flex font-semibold h-12 items-center w-max mt-6 justify-center overflow-hidden rounded-md bg-gradient-to-r from-[#fa8100] to-[#b05800] px-6 font-medium text-white transition-all duration-150 shadow-[3px_3px_8px_rgba(255,149,68,0.4)] active:translate-x-[3px] active:translate-y-[3px] active:[box-shadow:0px_0px_rgb(82_82_82)] active:bg-gradient-to-r active:from-[#fa8100] active:to-[#b05800] hover:bg-gradient-to-r hover:from-[#fa8100] hover:to-[#b05800] active:text-white"
-            >
-              {isSubmitting ? (
-                <span>Submitting...</span>
-              ) : (
-                <span>Submit</span>
-              )}
-              <div className="ml-1 transition group-hover:translate-x-2">
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 15 15"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                >
-                  <path
-                    d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
-                    fill="currentColor"
-                    // fill-rule="evenodd"
-                    // clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </div>
-            </Button>
+          >
+            {isSubmitting ? (
+              <span>Submitting...</span>
+            ) : (
+              <span>Submit</span>
+            )}
+            <div className="ml-1 transition group-hover:translate-x-2">
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+              >
+                <path
+                  d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                  fill="currentColor"
+                  // fill-rule="evenodd"
+                  // clip-rule="evenodd"
+                ></path>
+              </svg>
+            </div>
+          </Button>
       </div>
     </div>
   );
