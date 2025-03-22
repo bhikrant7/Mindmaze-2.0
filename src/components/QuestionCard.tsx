db30135bb240dac0189d9f2c8a6b6af7b781bce2
TasRuhani <tasdeequerockz@gmail.com>
2025-03-23 04:58:58 +0530

    toaster

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -37,7 +37,7 @@ const QuestionCard = () => {
       );
       
       if (!isCorrect) {
-        toast.error("Wrong answer! Try Again");
+       
       } else {
         setIsSolved(true);
       }
