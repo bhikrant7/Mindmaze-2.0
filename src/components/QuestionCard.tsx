c79e44eef608156aa5eba0b26f98b334a9177fce
TasRuhani <tasdeequerockz@gmail.com>
2025-03-23 13:22:53 +0530

    wrong ans toaster and solved

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -35,21 +35,35 @@ const QuestionCard = () => {
         curr_quest?.user_answer,
         curr_quest?.correct_answer
       );
-      
+
       if (!isCorrect) {
-       
+        toast.error("Wrong Answer! Try again.", {
+          duration: 10000,
+          position: "top-center",
+          style: {
+            background: "rgba(19, 12, 28, 0.15)",
+            border: "1px solid #422d28",
+            color: "#ff4d4d",
+            padding: "12px 16px",
+            borderRadius: "8px",
+            backdropFilter: "blur(8px)",
+          },
+          iconTheme: {
+            primary: "#ff4d4d",
+            secondary: "#422d28",
+          },
+        });
       } else {
         setIsSolved(true);
       }
 
-      const newSubmission = await createSubmission(
+      await createSubmission(
         team?.id as UUID,
         curr_quest?.id,
         curr_quest?.user_answer as string,
         isCorrect,
         team?.team_name as string
       );
-      console.log("newSubmission: ", newSubmission);
     } catch (error) {
       console.error(error);
     } finally {
@@ -89,41 +103,40 @@ const QuestionCard = () => {
         </div>
 
         {isSolved ? (
-          <p className="text-green-500 text-2xl font-bold">Solved</p>
+          <p className="text-green-500 text-9xl font-bold">Solved</p>
         ) : (
-          <Input
-            value={curr_quest?.user_answer || ""}
-            width="30%"
-            type="string"
-            onChange={(e) => setCurrAnswer(e.target.value)}
-            className="max-w-[20rem] py-6 ring-offset-[#FF9544] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#FF9544] focus-visible:ring-offset-2 dark:bg-zinc-950 border border-[#FF9544] focus:border-[#FF9544]"
-            placeholder="Type your answer here..."
-          />
-        )}
+          <>
+            <Input
+              value={curr_quest?.user_answer || ""}
+              type="string"
+              onChange={(e) => setCurrAnswer(e.target.value)}
+              className="max-w-[20rem] py-6 ring-offset-[#FF9544] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#FF9544] focus-visible:ring-offset-2 dark:bg-zinc-950 border border-[#FF9544] focus:border-[#FF9544]"
+              placeholder="Type your answer here..."
+            />
 
-        {!isSolved && (
-          <Button
-            onClick={handleSubmit}
-            disabled={isSubmitting}
-            className="group relative inline-flex h-12 items-center w-auto min-w-[150px] max-w-[150px] justify-center overflow-hidden rounded-md bg-gradient-to-r from-[#fa8100] to-[#b05800] px-6 font-medium text-white transition-all duration-150 shadow-[5px_5px_5px_rgba(255,149,68,0.4)] active:translate-x-[3px] active:translate-y-[3px] active:[box-shadow:0px_0px_rgb(82_82_82)] active:bg-gradient-to-r active:from-[#fa8100] active:to-[#b05800] hover:bg-gradient-to-r hover:from-[#fa8100] hover:to-[#b05800] active:text-white"
-          >
-            {isSubmitting ? <span>Submitting...</span> : <span>Submit</span>}
-            <div className="ml-1 transition group-hover:translate-x-2">
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 15 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-              >
-                <path
-                  d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </div>
-          </Button>
+            <Button
+              onClick={handleSubmit}
+              disabled={isSubmitting}
+              className="group relative inline-flex h-12 items-center w-auto min-w-[150px] max-w-[150px] justify-center overflow-hidden rounded-md bg-gradient-to-r from-[#fa8100] to-[#b05800] px-6 font-medium text-white transition-all duration-150 shadow-[5px_5px_5px_rgba(255,149,68,0.4)] active:translate-x-[3px] active:translate-y-[3px] active:[box-shadow:0px_0px_rgb(82_82_82)] active:bg-gradient-to-r active:from-[#fa8100] active:to-[#b05800] hover:bg-gradient-to-r hover:from-[#fa8100] hover:to-[#b05800] active:text-white"
+            >
+              {isSubmitting ? <span>Submitting...</span> : <span>Submit</span>}
+              <div className="ml-1 transition group-hover:translate-x-2">
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                >
+                  <path
+                    d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+            </Button>
+          </>
         )}
       </div>
     </div>
